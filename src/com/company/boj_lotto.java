f47dc70fdfd7a1af41f5208bfb909dbfa2858279
haewon-park <qkrgo_2@naver.com>
2021-04-13 20:50:40 +0900

    lotto 추가

--- src/com/company/boj_lotto.java
+++ src/com/company/boj_lotto.java
@@ -1,4 +1,50 @@
 package com.company;
 
+import java.io.*;
+
 public class boj_lotto {
+    static String[] line;
+    static boolean[] visit;
+    static int n;
+
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while(true) {
+            line = br.readLine().split("\\s");
+            n = Integer.parseInt(line[0]);
+            visit = new boolean[n+1];
+
+            if(n == 0) {
+                break;
+            }
+
+            dfs(1, 0);
+
+            bw.write( "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void dfs(int v, int d) throws IOException {
+        if(d == 6) {
+            String str = "";
+            for(int i = 1; i <= n; i++) {
+                if(visit[i]) str += line[i] + " ";
+            }
+            bw.write(str + "\n");
+            return;
+        }
+
+        for(int i = v; i <= n; i++) {
+            visit[i] = true;
+            dfs(i + 1, d + 1);
+            visit[i] = false;
+        }
+    }
 }
