ea3b7bf62a9e060182f390c664dde2d34fc3c182
haewon-park <65820741+haewon-park@users.noreply.github.com>
2021-07-19 20:09:10 +0900

    1325 시간초과

--- src/com/company/boj_1325.java
+++ src/com/company/boj_1325.java
@@ -1,4 +1,65 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class boj_1325 {
+    static int[] computers = new int[10001];
+    static ArrayList<Integer> list[];
+    static boolean visited[];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList[N + 1];
+        for(int i = 1; i <= N; i++){
+            list[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int computerA = Integer.parseInt(st.nextToken());
+            int computerB = Integer.parseInt(st.nextToken());
+            list[computerA].add(computerB);
+        }
+
+//        computers = new int[N + 1];
+        for(int i = 1; i <= N; i++){
+            visited = new boolean[N + 1];
+            dfs(i);
+        }
+
+        int max = 0;
+        for(int i = 1; i <= N; i++){
+            max = Math.max(max, computers[i]);
+        }
+
+        for(int i = 1; i <= N; i++){
+            if(computers[i] == max){
+//                bw.write(i + " ");
+                System.out.println(i + " ");
+            }
+        }
+
+//        br.close();
+//        bw.flush();
+//        bw.close();
+    }
+
+    static void dfs(int start) {
+        visited[start] = true;
+
+        for(int node : list[start]){
+            if(!visited[node]){
+                dfs(node);
+                computers[node]++;
+            }
+        }
+    }
 }
--- src/com/company/taegyu.java
+++ src/com/company/taegyu.java
@@ -1,25 +1,11 @@
 package com.company;
 
-public class taegyu {
+class Print{
+
+}
+
+public class taegyu{
     public static void main(String[] args) {
-        double result1 = 10 * 10 * Ccalculator.pi;
-        int result2 = Ccalculator.plus(10, 1);
-        int result3 = Ccalculator.minus(10, 1);
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
+
     }
-
-    public static class Ccalculator{
-        static double pi = 3.14159;
-
-        static int plus(int x, int y){
-            return x + y;
-        }
-
-        static int minus(int x, int y){
-            return x - y;
-        }
-    }
-
 }
