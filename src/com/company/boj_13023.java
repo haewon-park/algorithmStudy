2ccd1d453a9041ee30dad476e6fab8da4627aa5f
haewon-park <65820741+haewon-park@users.noreply.github.com>
2021-07-19 21:24:56 +0900

    solve 13023

--- src/com/company/boj_13023.java
+++ src/com/company/boj_13023.java
@@ -1,4 +1,67 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class boj_13023 {
+    static ArrayList<Integer>[] person;
+    static boolean[] visited;
+    static int result;
+
+    static void dfs(int i, int depth){
+        if(depth == 5 || result == 1){
+            result = 1;
+            return;
+        }
+
+        visited[i] = true;
+
+        for(int j = 0; j < person[i].size(); j++){
+            if(!visited[person[i].get(j)]){
+                visited[person[i].get(j)] = true;
+                dfs(person[i].get(j), depth + 1);
+                visited[person[i].get(j)] = false;
+            }
+        }
+
+        visited[i] = false;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        person = new ArrayList[N];
+        visited = new boolean[N];
+
+        for(int i = 0; i < N; i++){
+            person[i] = new ArrayList<>();
+        }
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+            int personA = Integer.parseInt(st.nextToken());
+            int personB = Integer.parseInt(st.nextToken());
+            person[personA].add(personB);
+            person[personB].add(personA);
+        }
+
+        result = 0;
+        for(int i = 0; i < N; i++){
+            dfs(i, 1);
+            if(result == 1){
+                break;
+            }
+        }
+
+        bw.write(result + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
 }
